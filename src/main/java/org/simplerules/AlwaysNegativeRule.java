697054855dd1a1eb1b9d5cb212656ba69d6727d3
Jesse Op 't Eynde <jesse.opteynde@student.kuleuven.be>
2025-11-18 19:36:31 +0100

    Changed to reflect new api

--- src/main/java/org/simplerules/AlwaysNegativeRule.java
+++ src/main/java/org/simplerules/AlwaysNegativeRule.java
@@ -5,12 +5,18 @@ import org.zaproxy.addon.reportingproxy.api.ReportingRule;
 import org.zaproxy.addon.reportingproxy.api.ReportingRuleFailure;
 import org.zaproxy.addon.reportingproxy.api.ReportingRuleResult;
 
+
 import java.util.List;
 
 public class AlwaysNegativeRule implements ReportingRule {
 
     @Override
-    public ReportingRuleResult handleHttpMessage(HttpMessage msg) {
+    public ReportingRuleResult handleHttpMessage(HttpMessage msg, boolean isSendRequest) {
         return new ReportingRuleFailure(this, msg, List.of(msg), "I always trigger!");
+    }
+
+    @Override
+    public String getRuleDisplayName() {
+        return "Always Negative";
     }
 }
--- src/main/java/org/simplerules/AlwaysPostiveRule.java
+++ src/main/java/org/simplerules/AlwaysPostiveRule.java
@@ -1,4 +1,5 @@
 package org.simplerules;
+import org.parosproxy.paros.network.HttpMessage;
 import org.zaproxy.addon.reportingproxy.api.ReportingRule;
 import org.zaproxy.addon.reportingproxy.api.ReportingRuleResult;
 import org.zaproxy.addon.reportingproxy.api.ReportingRuleSuccess;
@@ -6,7 +7,12 @@ import org.zaproxy.addon.reportingproxy.api.ReportingRuleSuccess;
 public class AlwaysPostiveRule implements ReportingRule {
 
     @Override
-    public ReportingRuleResult handleHttpMessage(org.parosproxy.paros.network.HttpMessage msg) {
+    public ReportingRuleResult handleHttpMessage(org.parosproxy.paros.network.HttpMessage msg, boolean isSendRequest) {
         return new ReportingRuleSuccess(this);
+    }
+
+    @Override
+    public String getRuleDisplayName() {
+        return "Always Positive";
     }
 }
